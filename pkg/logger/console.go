6b961d60fd02258821fb8c91731e2fe102e40445
Marius Bippig <marius.bippig@plentymarkets.com>
2026-01-07 16:19:56 +0100

    fix(Console Logger): Emergency log was logged with level panic

--- pkg/logger/console.go
+++ pkg/logger/console.go
@@ -16,7 +16,7 @@ func NewConsoleLogger(logLevel int) Logger {
 
 // Emergency logs a panic-level message and terminates the program.
 func (cl ConsoleLogger) Emergency(message string) {
-	panic(fmt.Sprintf("[PANIC] %s", message))
+	panic(fmt.Sprintf("[EMERGENCY] %s", message))
 }
 
 // Critical logs a critical-level message if the log level permits.
